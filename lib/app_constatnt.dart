731f4eeb0efc8ad43e67b1cb7d9f102286039687
Vazgen2k4 <Vazgen2k4@gmail.com>
2023-07-26 04:11:41 +0500

    Добавление анимаций

---
+++ lib/app_constatnt.dart
@@ -0,0 +1,5 @@
+abstract final class AppConstants {
+  static const ballAnimationDuration = Duration(seconds: 3);
+  static const errorMessage =
+      'Не могу наладить свяь с космосом (проверьте подключение интернета)';
+}
